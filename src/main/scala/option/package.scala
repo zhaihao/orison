12b4dec7754f52d4bb0300688c2d4ac49837179d
zhaihao <zhaihao@ooon.me>
2019-04-01 15:02:08 +0800

    feat:cli

---
+++ src/main/scala/option/package.scala
@@ -0,0 +1,18 @@
+import scala.language.implicitConversions
+/*
+ * Copyright (c) 2019.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit http://ooon.me or mail to zhaihao@ooon.me
+ */
+
+/**
+  * package
+  *
+  * @author zhaihao
+  * @version 1.0
+  * @since 2019-04-01 14:49
+  */
+package object option {
+  implicit def anyToOption[T](t: T) = Option(t)
+}
---
+++ src/test/scala/option/OptionSpec.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit http://ooon.me or mail to zhaihao@ooon.me
+ */
+
+package option
+import test.BaseSpec
+
+/**
+  * OptionSpec
+  *
+  * @author zhaihao
+  * @version 1.0
+  * @since 2019-03-21 17:19
+  */
+class OptionSpec extends BaseSpec {
+  def test1(a: Option[Int])    = a.get * 2
+  def test2(b: Option[String]) = b.get.reverse
+
+  "any to option" in {
+    test1(2)     ==> 4
+    test2("abc") ==> "cba"
+  }
+}
