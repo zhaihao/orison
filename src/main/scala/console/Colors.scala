ef16847fca16363f6248341d796ef006770a966a
zhaihao <zhaihao@ooon.me>
2019-04-01 11:11:11 +0800

    feat:logback 彩色level

---
+++ src/main/scala/console/Colors.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit http://ooon.me or mail to zhaihao@ooon.me
+ */
+
+package console
+
+/**
+  * Colors
+  *
+  * @author zhaihao
+  * @version 1.0 21/03/2018 16:35
+  */
+object Colors {
+
+  import scala.Console._
+
+  lazy val isANSISupported = {
+    sys.props
+      .get("sbt.log.noformat")
+      .map(_ != "true")
+      .orElse {
+        sys.props
+          .get("os.name")
+          .map(_.toLowerCase(java.util.Locale.ENGLISH))
+          .filter(_.contains("windows"))
+          .map(_ => false)
+      }
+      .getOrElse(true)
+  }
+
+  def red(str: String): String = if (isANSISupported) RED + str + RESET else str
+
+  def blue(str: String): String =
+    if (isANSISupported) BLUE + str + RESET else str
+
+  def cyan(str: String): String =
+    if (isANSISupported) CYAN + str + RESET else str
+
+  def green(str: String): String =
+    if (isANSISupported) GREEN + str + RESET else str
+
+  def magenta(str: String): String =
+    if (isANSISupported) MAGENTA + str + RESET else str
+
+  def white(str: String): String =
+    if (isANSISupported) WHITE + str + RESET else str
+
+  def black(str: String): String =
+    if (isANSISupported) BLACK + str + RESET else str
+
+  def yellow(str: String): String =
+    if (isANSISupported) YELLOW + str + RESET else str
+
+}
---
+++ src/main/scala/log/logback/ColoredLevel.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit http://ooon.me or mail to zhaihao@ooon.me
+ */
+
+package log.logback
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.pattern.ClassicConverter
+import ch.qos.logback.classic.spi.ILoggingEvent
+import console.Colors
+
+/**
+  * ColoredLevel
+  * {{{
+  *   %coloredLevel %logger{15} - %message%n%xException{5}
+  * }}}
+  *
+  * @author zhaihao
+  * @version 1.0 21/03/2018 16:32
+  */
+class ColoredLevel extends ClassicConverter {
+
+  def convert(event: ILoggingEvent): String = {
+    event.getLevel match {
+      case Level.TRACE => "[" + Colors.blue("trace") + "]"
+      case Level.DEBUG => "[" + Colors.cyan("debug") + "]"
+      case Level.INFO  => "[" + Colors.green("info") + " ]"
+      case Level.WARN  => "[" + Colors.yellow("warn") + " ]"
+      case Level.ERROR => "[" + Colors.red("error") + "]"
+    }
+  }
+
+}
