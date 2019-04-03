060da48182387b39067e9e16345297602bc2e588
zhaihao <zhaihao@ooon.me>
2019-04-03 16:18:26 +0800

    feat:remove csv

--- src/main/scala/csv/MalformedCSVException.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019.
- * OOON.ME ALL RIGHTS RESERVED.
- * Licensed under the Mozilla Public License, version 2.0
- * Please visit http://ooon.me or mail to zhaihao@ooon.me
- */
-package csv
-
-class MalformedCSVException(message: String) extends Exception(message)
