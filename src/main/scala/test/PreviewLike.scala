2bf448b76cc7688e1d3b377771a44e0cf9dfcf3c
zhaihao <zhaihao@ooon.me>
2019-04-10 15:51:55 +0800

    feat:PreviewLike 支持 windows 系统

--- src/main/scala/test/PreviewLike.scala
+++ src/main/scala/test/PreviewLike.scala
@@ -21,9 +21,18 @@ import scala.language.postfixOps
   */
 trait PreviewLike {
 
-  require(sys.props("os.name").toLowerCase.contains("os x"), "预览文件功能只支持 os x 系统")
+  def preview(file: File): Unit = preview(file.getPath)
 
-  def preview(file: File) = s"open ${file.getPath}" !
+  /**
+    * 没有在windows系统上进行测试
+    *
+    * @param path file
+    * @return
+    */
+  def preview(path: String): Unit = system.osName.toLowerCase match {
+    case name if name.contains("os x")    => s"open $path" !
+    case name if name.contains("windows") => s"start /b /max $path" !
+    case _                                => throw new Exception(s"Preview function do not support your os: ${system.osName}")
+  }
 
-  def preview(path: String) = s"open $path" !
 }
