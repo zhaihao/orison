b01c7c0d6dc6973a52cc79b4f16eea708a9d11da
zhaihao <zhaihao@ooon.me>
2019-08-30 10:23:09 +0800

    feat: update plot vega version

--- src/main/scala/plot/HtmlRenderer.scala
+++ src/main/scala/plot/HtmlRenderer.scala
@@ -31,9 +31,9 @@ class HtmlRenderer(vega: Vega) extends StrictLogging {
   private val head = s"""|<head>
                          |    <meta charset="utf-8">
                          |    <title>scala plot</title>
-                         |    <script src="https://cdn.jsdelivr.net/npm/vega@$VEGA_VERSION"></script>
-                         |    <script src="https://cdn.jsdelivr.net/npm/vega-lite@$VEGA_LITE_VERSION"></script>
-                         |    <script src="https://cdn.jsdelivr.net/npm/vega-embed@$VEGA_EMBED"></script>
+                         |    <script src="https://cdn.jsdelivr.net/npm/vega@5"></script>
+                         |    <script src="https://cdn.jsdelivr.net/npm/vega-lite@4"></script>
+                         |    <script src="https://cdn.jsdelivr.net/npm/vega-embed@4"></script>
                          |</head>
                          |""".stripMargin
 
