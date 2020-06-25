9558e8e5a47400a42768bba587d8a1762eed4c6c
zhaihao <zhaihao@ooon.me>
2020-06-26 00:44:31 +0800

    FEAT: logback logger 灰色

---
+++ src/main/scala/log/logback/ColoredLogger.scala
@@ -0,0 +1,43 @@
+package log.logback
+
+import ch.qos.logback.classic.pattern.{
+  Abbreviator,
+  ClassNameOnlyAbbreviator,
+  ClassicConverter,
+  TargetLengthBasedClassNameAbbreviator
+}
+import ch.qos.logback.classic.spi.{CallerData, ILoggingEvent}
+
+import scala.Console._
+
+/**
+  * ColoredLogger
+  *
+  * @author zhaihao
+  * @version 1.0
+  * @since 2020/6/26 00:43
+  */
+
+class ColoredLogger extends ClassicConverter {
+  private[this] var abbreviator: Abbreviator = _
+
+  override def start() = {
+    val optStr = getFirstOption
+    if (optStr != null) try {
+      val targetLen = optStr.toInt
+      if (targetLen == 0) abbreviator = new ClassNameOnlyAbbreviator()
+      else if (targetLen > 0) abbreviator = new TargetLengthBasedClassNameAbbreviator(targetLen)
+    } catch {
+      case _: NumberFormatException =>
+      // FIXME: better error reporting
+    }
+  }
+
+  override def convert(event: ILoggingEvent) = {
+    val fqn    = event.getLoggerName
+    val cda    = event.getCallerData
+    val line   = if (cda != null && cda.nonEmpty) cda(0).getLineNumber.toString else CallerData.NA
+    val logger = if (abbreviator == null) fqn else abbreviator.abbreviate(fqn)
+    WHITE + logger + "(" + line + ")" + RESET
+  }
+}
