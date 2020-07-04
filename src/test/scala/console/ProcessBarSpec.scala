142094254c676e928d8ece9aba071368963c48e5
zhaihao <zhaihao@ooon.me>
2020-07-04 22:28:14 +0800

    FEAT: process bar

---
+++ src/test/scala/console/ProcessBarSpec.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit <http://ooon.me> or mail to zhaihao@ooon.me
+ */
+
+package console
+
+import config.HConfig
+import test.BaseSpec
+
+import scala.util.Random
+
+/**
+  * ProcessBarSpec
+  *
+  * @author zhaihao
+  * @version 1.0
+  * @since 2020/7/4 22:15
+  */
+object ProcessBarSpec extends orison.App with HConfig {
+  val its = 100
+
+  val progress =
+    ProgressBar(its, new BarFormatter(ncols = 100, unit = "file") with OrdersOfMagnitudeScaling with UnicodeBarFormat)
+  progress meter { updater =>
+    (1 to its).foreach { _ =>
+      Thread.sleep(Random.nextInt(1000))
+      updater.update(1)
+    }
+  }
+}
