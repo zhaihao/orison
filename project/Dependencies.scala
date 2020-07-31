fa398444e0b6fe6ef5e05d7c306144381bbd1fe1
zhaihao <zhaihao@ooon.me>
2020-07-31 09:56:29 +0800

    BUILD: Update json4s-jackson from 3.6.7 to 3.6.9 (#11)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies extends AutoPlugin {
     lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec" % "3.2.0" % Provided
     lazy val play_json       = "com.typesafe.play" %% "play-json"          % "2.6.14" % Provided
     lazy val os_lib          = "com.lihaoyi"       %% "os-lib"             % "0.7.1" % Provided
-    lazy val json4s          = "org.json4s"        %% "json4s-jackson"     % "3.6.7" % Provided
+    lazy val json4s          = "org.json4s"        %% "json4s-jackson"     % "3.6.9" % Provided
     lazy val typesafe_config = "com.typesafe"      % "config"              % "1.3.4" % Provided
     lazy val argon2          = "de.mkammerer"      % "argon2-jvm"          % "2.7"   % Provided
 
