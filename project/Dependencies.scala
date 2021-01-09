c225d14d730c9b1e8c8c9afc6ff75824cd93068d
zhaihao <zhaihao@ooon.me>
2021-01-09 00:56:10 +0000

    BUILD: Update play-json from 2.9.1 to 2.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies extends AutoPlugin {
 
   object autoImport {
     lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec" % "3.2.3"  % Provided
-    lazy val play_json       = "com.typesafe.play" %% "play-json"          % "2.9.1"  % Provided
+    lazy val play_json       = "com.typesafe.play" %% "play-json"          % "2.9.2"  % Provided
     lazy val os_lib          = "com.lihaoyi"       %% "os-lib"             % "0.7.1"  % Provided
     lazy val json4s          = "org.json4s"        %% "json4s-jackson"     % "3.6.10" % Provided
     lazy val typesafe_config = "com.typesafe"      % "config"              % "1.4.1"  % Provided
