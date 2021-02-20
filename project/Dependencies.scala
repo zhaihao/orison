d5ff23cebfebfcbef1f9e3c2b12e635a74a11f2f
zhaihao <zhaihao@ooon.me>
2021-02-20 00:33:18 +0000

    BUILD: Update scalatest-freespec from 3.2.3 to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies extends AutoPlugin {
   override def trigger  = allRequirements
 
   object autoImport {
-    lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec" % "3.2.3"  % Provided
+    lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec" % "3.2.5"  % Provided
     lazy val play_json       = "com.typesafe.play" %% "play-json"          % "2.9.2"  % Provided
     lazy val os_lib          = "com.lihaoyi"       %% "os-lib"             % "0.7.2"  % Provided
     lazy val json4s          = "org.json4s"        %% "json4s-jackson"     % "3.6.10" % Provided
