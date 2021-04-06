0ebcade58fdd70f6e61a32032a8a1a711ba75d46
zhaihao <zhaihao@ooon.me>
2021-04-06 10:19:17 +0800

    BUILD: Update os-lib from 0.7.3 to 0.7.4 (#52)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies extends AutoPlugin {
   object autoImport {
     lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec" % "3.2.6"  % Provided
     lazy val play_json       = "com.typesafe.play" %% "play-json"          % "2.9.2"  % Provided
-    lazy val os_lib          = "com.lihaoyi"       %% "os-lib"             % "0.7.3"  % Provided
+    lazy val os_lib          = "com.lihaoyi"       %% "os-lib"             % "0.7.4"  % Provided
     lazy val json4s          = "org.json4s"        %% "json4s-jackson"     % "3.6.11" % Provided
     lazy val typesafe_config = "com.typesafe"      % "config"              % "1.4.1"  % Provided
     lazy val squants         = "org.typelevel"     %% "squants"            % "1.7.4"  % Provided
