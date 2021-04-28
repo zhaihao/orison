520b2b2ce9e7a7bc053c10fa4a0fce2d8765eb21
zhaihao <zhaihao@ooon.me>
2021-04-28 18:47:23 +0800

    feat:timed label 放到后面

--- src/main/scala/util/package.scala
+++ src/main/scala/util/package.scala
@@ -8,9 +8,9 @@ import com.typesafe.scalalogging.Logger
   * @since 2020/5/19 6:34 下午
   */
 package object util {
-  def timed[T](label: String, log: Logger)(t: => T): T = {
-    val start = System.nanoTime
-    val result = t
+  def timed[T](log: Logger, label: String = "")(t: => T): T = {
+    val start   = System.nanoTime
+    val result  = t
     val elapsed = System.nanoTime - start
     log.debug(label + " took " + (elapsed / 1e6) + " ms")
     result
