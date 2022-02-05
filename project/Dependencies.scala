b29067216b17595b5e74e742f23abf8e415097c1
zhaihao <zhaihao@ooon.me>
2022-02-05 00:30:25 +0000

    🛠 Update os-lib 0.8.0 -> 0.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies extends AutoPlugin {
     lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec"     % "3.2.10" % Provided
     lazy val scalatest_must  = "org.scalatest"     %% "scalatest-mustmatchers" % "3.2.10" % Provided
     lazy val play_json       = "com.typesafe.play" %% "play-json"              % "2.9.2"  % Provided
-    lazy val os_lib          = "com.lihaoyi"       %% "os-lib"                 % "0.8.0"  % Provided
+    lazy val os_lib          = "com.lihaoyi"       %% "os-lib"                 % "0.8.1"  % Provided
     lazy val fastparse       = "com.lihaoyi"       %% "fastparse"              % "2.3.3"  % Provided
     lazy val json4s          = "org.json4s"        %% "json4s-jackson"         % "4.0.3"  % Provided
     lazy val typesafe_config = "com.typesafe"       % "config"                 % "1.4.1"  % Provided
