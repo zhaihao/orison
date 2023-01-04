3489e3a177d94acdea48347d34dacf09ec9f99ef
zhaihao <zhaihao@ooon.me>
2023-01-04 22:10:36 +0800

    fix: 移除 jvm discriminator

--- src/main/scala/log/logback/JvmDiscriminator.scala
+++ src/main/scala/log/logback/JvmDiscriminator.scala
@@ -1,43 +1,43 @@
-package log.logback
-
-import ch.qos.logback.classic.spi.ILoggingEvent
-import ch.qos.logback.core.sift.AbstractDiscriminator
-import sun.jvmstat.monitor.{MonitoredHost, MonitoredVmUtil, VmIdentifier}
-
-/** JvmDiscriminator
-  *
-  * @author
-  *   zhaihao
-  * @version 1.0
-  * @since 2020/12/11
-  *   5:21 下午
-  */
-class JvmDiscriminator extends AbstractDiscriminator[ILoggingEvent] {
-  var jvmName: String = _
-
-  override def start() = {
-//    val pid = _root_.system.pid.toInt
-    val pid = ProcessHandle.current.pid
-    jvmName = JvmDiscriminator.jvmName(pid).getOrElse("application")
-    started = true
-  }
-
-  override def getDiscriminatingValue(e: ILoggingEvent) = jvmName
-
-  override def getKey = "jvmName"
-}
-
-object JvmDiscriminator {
-  val regex = """.*org\.scalatest\.tools\.Runner -s .+(Jvm\d+).*""".r
-
-  def jvmName(pid: Long) = {
-    val vmID    = new VmIdentifier("//" + pid + "?mode=r")
-    val mh      = MonitoredHost.getMonitoredHost("localhost")
-    val vm      = mh.getMonitoredVm(vmID)
-    val command = MonitoredVmUtil.commandLine(vm)
-    command match {
-      case regex(name) => Some(name)
-      case _           => None
-    }
-  }
-}
+//package log.logback
+//
+//import ch.qos.logback.classic.spi.ILoggingEvent
+//import ch.qos.logback.core.sift.AbstractDiscriminator
+//import sun.jvmstat.monitor.{MonitoredHost, MonitoredVmUtil, VmIdentifier}
+//
+///** JvmDiscriminator
+//  *
+//  * @author
+//  *   zhaihao
+//  * @version 1.0
+//  * @since 2020/12/11
+//  *   5:21 下午
+//  */
+//class JvmDiscriminator extends AbstractDiscriminator[ILoggingEvent] {
+//  var jvmName: String = _
+//
+//  override def start() = {
+////    val pid = _root_.system.pid.toInt
+//    val pid = ProcessHandle.current.pid
+//    jvmName = JvmDiscriminator.jvmName(pid).getOrElse("application")
+//    started = true
+//  }
+//
+//  override def getDiscriminatingValue(e: ILoggingEvent) = jvmName
+//
+//  override def getKey = "jvmName"
+//}
+//
+//object JvmDiscriminator {
+//  val regex = """.*org\.scalatest\.tools\.Runner -s .+(Jvm\d+).*""".r
+//
+//  def jvmName(pid: Long) = {
+//    val vmID    = new VmIdentifier("//" + pid + "?mode=r")
+//    val mh      = MonitoredHost.getMonitoredHost("localhost")
+//    val vm      = mh.getMonitoredVm(vmID)
+//    val command = MonitoredVmUtil.commandLine(vm)
+//    command match {
+//      case regex(name) => Some(name)
+//      case _           => None
+//    }
+//  }
+//}
