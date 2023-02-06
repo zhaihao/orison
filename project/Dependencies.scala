84a273f8258ec119d4688c0633fe420cdaefa0bc
zhaihao <zhaihao@ooon.me>
2023-02-06 07:05:08 +0000

    update: slick-pg, slick-pg_jts_lt, ... 0.20.4 -> 0.21.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,9 +38,9 @@ object Dependencies extends AutoPlugin {
 
     lazy val slick = Seq(
       "com.typesafe.slick"  %% "slick"              % "3.4.1"  % Provided,
-      "com.github.tminglei" %% "slick-pg"           % "0.20.4" % Provided,
-      "com.github.tminglei" %% "slick-pg_play-json" % "0.20.4" % Provided,
-      "com.github.tminglei" %% "slick-pg_jts_lt"    % "0.20.4" % Provided
+      "com.github.tminglei" %% "slick-pg"           % "0.21.1" % Provided,
+      "com.github.tminglei" %% "slick-pg_play-json" % "0.21.1" % Provided,
+      "com.github.tminglei" %% "slick-pg_jts_lt"    % "0.21.1" % Provided
     )
 
     lazy val log = Seq(
