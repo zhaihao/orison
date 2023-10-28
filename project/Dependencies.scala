bbc6ec3a17614dd4e0d3a177849ff94fff78d017
zhaihao <zhaihao@ooon.me>
2023-10-28 00:41:07 +0000

    update: play-json 2.10.1 -> 2.10.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies extends AutoPlugin {
   object autoImport {
     lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec"     % "3.2.17" % Provided
     lazy val scalatest_must  = "org.scalatest"     %% "scalatest-mustmatchers" % "3.2.17" % Provided
-    lazy val play_json       = "com.typesafe.play" %% "play-json"              % "2.10.1" % Provided
+    lazy val play_json       = "com.typesafe.play" %% "play-json"              % "2.10.2" % Provided
     lazy val os_lib          = "com.lihaoyi"       %% "os-lib"                 % "0.9.0"  % Provided
     lazy val fastparse       = "com.lihaoyi"       %% "fastparse"              % "2.3.3"  % Provided
     lazy val json4s          = "org.json4s"        %% "json4s-jackson"         % "4.0.6"  % Provided
