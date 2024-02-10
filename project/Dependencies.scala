f38e8c709b8e0c26cbec8a23b9020b35271e79f5
zhaihao <zhaihao@ooon.me>
2024-02-10 00:40:22 +0000

    update: scalatest-freespec, ... 3.2.17 -> 3.2.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,8 +19,8 @@ object Dependencies extends AutoPlugin {
   override def trigger  = allRequirements
 
   object autoImport {
-    lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec"     % "3.2.17" % Provided
-    lazy val scalatest_must  = "org.scalatest"     %% "scalatest-mustmatchers" % "3.2.17" % Provided
+    lazy val scalatest       = "org.scalatest"     %% "scalatest-freespec"     % "3.2.18" % Provided
+    lazy val scalatest_must  = "org.scalatest"     %% "scalatest-mustmatchers" % "3.2.18" % Provided
     lazy val play_json       = "org.playframework" %% "play-json"              % "3.0.2"  % Provided
     lazy val os_lib          = "com.lihaoyi"       %% "os-lib"                 % "0.9.3"  % Provided
     lazy val fastparse       = "com.lihaoyi"       %% "fastparse"              % "2.3.3"  % Provided
