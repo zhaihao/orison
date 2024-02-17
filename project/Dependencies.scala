0719489deac4d827afc52c8c14060ebf3b499baf
zhaihao <zhaihao@ooon.me>
2024-02-17 08:44:12 +0800

    update: logback-classic 1.4.14 -> 1.5.0 (#192)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,8 +44,8 @@ object Dependencies extends AutoPlugin {
     )
 
     lazy val log = Seq(
-      "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"  % Provided,
-      "ch.qos.logback"              % "logback-classic" % "1.4.14" % Provided
+      "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5" % Provided,
+      "ch.qos.logback"              % "logback-classic" % "1.5.0" % Provided
     )
 
     val excludes = Seq()
